7915172a6f452d2650aefb8a618ff576ba4c49d9
hey-awais <64176871+hey-awais@users.noreply.github.com>
2022-06-09 14:11:17 +0500

    data

--- src/utils/ApiUrl.js
+++ src/utils/ApiUrl.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://stakingpoolbuilderapi.quecko.com'
-// export const API_URL = 'http://192.168.18.143:8080' 
+// export const API_URL = 'https://stakingpoolbuilderapi.quecko.com'
+export const API_URL = "http://192.168.18.40:8080";
 // backendUrl : "http://ec2-54-202-62-179.us-west-2.compute.amazonaws.com:8080"
